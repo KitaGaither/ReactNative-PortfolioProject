e7543365886b1ad50b00596bd3d1798b7afa1edf
Lakita Gaither <lakitagaither@yahoo.com>
2021-08-06 22:52:44 -0500

    Icons

--- components/main.js
+++ components/main.js
@@ -4,10 +4,41 @@ import Services from '../components/services';
 import About from '../components/about';
 import Contact from '../components/contact';
 import Constants from 'expo-constants';
-import { View, Platform } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { View, Platform, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator} from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
+    stackIcon: {
+        marginLeft: 10,
+        color: '#fff',
+        fontSize: 24
+    }
+});
+
 
 const ContactNavigator = createStackNavigator(
   {
@@ -21,7 +52,12 @@ const ContactNavigator = createStackNavigator(
           headerTintColor: '#fff',
           headerTitleStyle: {
               color: '#fff'
-          }
+          },
+          headerLeft: <Icon
+              name='address-card'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+          />
       }
   }
 );
@@ -38,7 +74,12 @@ const AboutNavigator = createStackNavigator(
           headerTintColor: '#fff',
           headerTitleStyle: {
               color: '#fff'
-          }
+          },
+          headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                />
       }
   }
 );
@@ -56,7 +97,12 @@ const ServicesNavigator = createStackNavigator(
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
+            },
+            headerLeft: <Icon
+              name='info-circle'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}         
+            />
         }
     }
 );
@@ -73,22 +119,80 @@ const HomeNavigator = createStackNavigator(
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
+            },
+            headerLeft: <Icon
+              name='home'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}  
+          />
         }
     }
 );
 
+
+
 const MainNavigator = createDrawerNavigator(
     {
-        Home: { screen: HomeNavigator },
-        Services: { screen: ServicesNavigator },
-        About: { screen: AboutNavigator },
-        Contact: { screen: ContactNavigator }
+        Home: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='home'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Services: {
+            screen: ServicesNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        About: {
+            screen: AboutNavigator,
+            navigationOptions: {
+                drawerLabel: 'About Us',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='info-circle'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Contact: {
+            screen: ContactNavigator,
+            navigationOptions: {
+                drawerLabel: 'Contact Us',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='address-card'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        }
     },
     {
-        drawerBackgroundColor: '#91BFF1'
+        drawerBackgroundColor: '#CEC8FF',
     }
 );
+
 
 const AppNavigator = createAppContainer(MainNavigator)
 
@@ -107,5 +211,8 @@ class Main extends Component {
         );
     }
 }
+
+
+
 
 export default Main;
